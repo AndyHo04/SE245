76b7e30a33af4130a7f0f05cafc7528a5f592484
Andy Ho <155758784+AndyHo04@users.noreply.github.com>
2024-04-09 09:57:19 -0400

    W3 Demo done

---
+++ W3Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3Demo{
+    class Program{
+        static void Main (string[] args){
+            
+            for (int i = 1; i <= 20; i++){
+               
+               for (int j = 1; j <= 20; j++){
+                    Console.Write($"{i * j}\t");
+               }
+
+               Console.WriteLine();
+            }
+            
+
+        }
+    }
+}
+
