9cb76468fc249087b90adc94965f02388b06a055
Andy Ho <155758784+AndyHo04@users.noreply.github.com>
2024-04-16 09:21:27 -0400

    W4D1 Demo done

---
+++ W4Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace W4Demo
+{
+    class Program
+    {   
+        struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        static Point GetRandomPoint()
+        {
+    
+            Random r = new Random();
+            Point p = new Point(r.Next(-100,100), r.Next(-100, 100));
+            return p;
+        }
+        static void Main(string[] args)
+        {
+            Point p = GetRandomPoint();
+            Console.WriteLine($"X: {p.X}, Y: {p.Y}");
+        }
+    }
+}
