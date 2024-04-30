e0d22210bbab957587c5a62125432bd544f16f50
Andy Ho <155758784+AndyHo04@users.noreply.github.com>
2024-04-30 09:22:35 -0400

    W6 Challenge done

---
+++ W6Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//write a program that checks whether a given string is a palindrome or not using a method that takes a string as input
+namespace W6Demo
+{
+    class Program
+    {
+        static bool IsPalindrome(string str)
+        {
+            string reversed = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i].ToString();
+            }
+            if (str == reversed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("hello"));
+            Console.WriteLine(IsPalindrome("racecar"));
+        }
+    }
+    
+}
+
