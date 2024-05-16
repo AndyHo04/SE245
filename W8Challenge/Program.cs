f1475b1a358e1f71d3cc4fed938b9f15f91e043f
Andy Ho <155758784+AndyHo04@users.noreply.github.com>
2024-05-16 08:41:22 -0400

    W8 Challenge done

---
+++ W8Challenge/Program.cs
@@ -0,0 +1,44 @@
+﻿//create a program that returns the length of the last word in the string
+
+namespace W8Challenge
+{
+    class Program
+    {
+        static int LengthOfLastWord(string str)
+        {
+            //ignore space at the end of the string
+            str = str.Trim();
+            string[] words = str.Split(' ');
+            return words[words.Length - 1].Length;
+        }
+
+        public static int FindLengthOfLastWord(string str)
+        {
+            int length = 0;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (str[i] == ' ')
+                {
+                    if (length > 0)
+                    {
+                        //return length;
+                        break;
+                    }
+                }
+                else
+                {
+                    length++;
+                }
+            }
+            return length;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("hello world"));
+            Console.WriteLine(LengthOfLastWord("hello"));
+            Console.WriteLine(LengthOfLastWord("hello world this is a test"));
+            Console.WriteLine(LengthOfLastWord("Amazing world of   dragon   "));
+            Console.WriteLine(FindLengthOfLastWord("Philospher stone of wisdom"));
+        }
+    }
+}
